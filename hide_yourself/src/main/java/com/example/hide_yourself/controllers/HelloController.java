0897d1dbef8a584edfbc81b1a6556fee64b62521
Yang Liu <yang.jace.liu@linux.com>
2023-05-27 13:17:59 -0600

    Add OpenAPI doc and configure generator, and a dummy controller (#5)
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

---
+++ hide_yourself/src/main/java/com/example/hide_yourself/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.hide_yourself.controllers;
+
+import com.example.hide_yourself.api.HelloApi;
+import com.example.hide_yourself.model.HelloWorld;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.OffsetDateTime;
+
+@Controller
+public class HelloController implements HelloApi {
+    @Override
+    public Mono<ResponseEntity<HelloWorld>> helloGet(ServerWebExchange exchange) {
+        return Mono.just(ResponseEntity.ok(HelloWorld.builder()
+                        .text("Hello World")
+                        .currentTime(OffsetDateTime.now())
+                .build()));
+    }
+}
