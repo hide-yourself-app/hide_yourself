f0dc71db1488285744498d68186c778179e1740c
Yang Liu <yang.jace.liu@linux.com>
2023-05-27 11:34:44 -0600

    Fix the tests configuration to make the project buildable (#1)
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- hide_yourself/src/test/java/com/example/hide_yourself/HideYourselfApplicationTests.java
+++ hide_yourself/src/test/java/com/example/hide_yourself/HideYourselfApplicationTests.java
@@ -1,9 +1,18 @@
 package com.example.hide_yourself;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {R2dbcAutoConfiguration.class})
 class HideYourselfApplicationTests {
 
 	@Test
