7d96e9f5c0a7b0ecd0f8de4438e21e42f2468109
Yang Liu <yang.jace.liu@linux.com>
2023-05-27 15:55:06 -0600

    Clean up models with annotations (#10)
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- hide_yourself/src/main/java/com/example/hide_yourself/models/UserWebsite.java
+++ hide_yourself/src/main/java/com/example/hide_yourself/models/UserWebsite.java
@@ -1,47 +1,15 @@
 package com.example.hide_yourself.models;
 
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserWebsite {
-    private long userId;
+    private Long userId;
     private String website;
     private String websiteLoginEmail;
     private String websiteLoginPassword;
-
-    public UserWebsite() {}
-
-    public UserWebsite(Account account, String website, String websiteLoginEmail) {
-        this.userId = account.getUserId();
-        this.website = website;
-        this.websiteLoginEmail = websiteLoginEmail;
-    }
-
-    public UserWebsite(Account account, String website, String websiteLoginEmail, String websiteLoginPassword) {
-        this.userId = account.getUserId();
-        this.website = website;
-        this.websiteLoginEmail = websiteLoginEmail;
-        this.websiteLoginPassword = websiteLoginPassword;
-    }
-
-    public long getUserId() {
-        return this.userId;
-    }
-
-    public String getWebsite() {
-        return this.website;
-    }
-
-    public String getWebsiteLoginEmail() {
-        return this.websiteLoginEmail;
-    }
-
-    public void setWebsiteLoginEmail(String websiteLoginEmail) {
-        this.websiteLoginEmail = websiteLoginEmail;
-    }
-
-    public String getWebsiteLoginPassword() {
-        return this.websiteLoginPassword;
-    }
-
-    public void setWebsiteLoginPassword(String password) {
-        this.websiteLoginPassword = password;
-    }
 }
