5fd796b7aff27c6d10e79f43b99d3a0d39020463
William Mao <tomcat_mao@hotmail.com>
2023-05-27 13:55:07 -0600

    Added formik and Yup

--- client/src/component/Register/index.js
+++ client/src/component/Register/index.js
@@ -1,55 +1,87 @@
-import React from "react";
-import "./_regiser.scss";
-import { IonIcon } from "@ionic/react";
+import React from 'react';
+import './_regiser.scss';
+import { IonIcon } from '@ionic/react';
+import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import {
   lockClosedOutline,
   mailOutline,
   closeOutline,
   personOutline,
-} from "ionicons/icons";
+} from 'ionicons/icons';
 
 export default function Register() {
+
+  let validationSchema = Yup.object().shape({
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().email('Invalid email format').required('Email is required'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+    confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Password must match').required('Confirm password is required'),
+  });
+
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+
   return (
-    <div className="wrapper">
-      <div className="form-box register">
+    <div className='wrapper'>
+      <div className='form-box login'>
         <h2>Registration</h2>
-        <form action="#">
-          <div className="input-box">
-            <span className="icon">
+        <form action={formik.handleSubmit}>
+          <div className='input-box'>
+            <span className='icon'>
               <IonIcon icon={personOutline} />
             </span>
-            <input type="text" required />
+            <input name='username' type='text' onChange={formik.handleChange('username')} required />
             <label>Username</label>
           </div>
-          <div className="input-box">
-            <span className="icon">
+          <div className='input-box'>
+            <span className='icon'>
               <IonIcon icon={mailOutline} />
             </span>
-            <input type="email" required />
+            <input name='email' type='email' onChange={formik.handleChange('email')} required />
             <label>Email</label>
           </div>
-
-          <div className="input-box">
-            <span className="icon">
+          <div className='input-box'>
+            <span className='icon'>
               <IonIcon icon={lockClosedOutline} />
             </span>
-            <input type="password" required />
+            <input name='password' type='password' onChange={formik.handleChange('password')} required />
             <label>Password</label>
           </div>
-          <div className="remember-forgot">
+          <div className='input-box'>
+            <span className='icon'>
+              <IonIcon icon={lockClosedOutline} />
+            </span>
+            <input name='confirmPassword' type='password' onChange={formik.handleChange('confirmPassword')} required />
+            <label>Confirm Password</label>
+          </div>
+          <div className='remember-forgot'>
             <label>
-              <input type="checkbox" />I agree to the terms & conditions
+              <input type='checkbox' />I agree to the terms & conditions
             </label>
           </div>
-          <button type="submit" className="btn">
+          <button type='submit' className='btn'>
             Register
           </button>
-          <div className="login-register">
+          <div className='login-register'>
             <p>
               Already have an account?
-              <a href="#" className="login-link">
+              <Link to='/' className='login-link'>
                 Login
-              </a>
+              </Link>
             </p>
           </div>
         </form>
