077309037bfc7d24a5cc0f45758cb015446ac8c7
Sean Zhu <zhuqingyun@outlook.com>
2023-05-27 12:18:12 -0600

    Update login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
+import "./App.css";
+import Header from "./component/Header";
+import Login from "./component/Login";
+import Wrapper from "./component/Wrapper";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <Header />
+      <Wrapper />
+      <Login />
+    </div>
   );
 }
 
