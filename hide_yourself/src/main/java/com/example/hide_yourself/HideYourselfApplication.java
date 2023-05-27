2165ceb85e1d38ff127a875d8116fd220666ae22
Mengqiu (Roger) Chen <rogermchen.ubc@gmail.com>
2023-05-27 11:07:06 -0600

    init commit

---
+++ hide_yourself/src/main/java/com/example/hide_yourself/HideYourselfApplication.java
@@ -0,0 +1,13 @@
+package com.example.hide_yourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideYourselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HideYourselfApplication.class, args);
+	}
+
+}
