835828cd6d1e6d4785da0e999cfa5b30cb12909a
Y. Jace Liu <yang.jace.liu@linux.com>
2023-05-27 15:22:23 -0600

    Clean up models with annotations
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- hide_yourself/src/main/java/com/example/hide_yourself/models/Account.java
+++ hide_yourself/src/main/java/com/example/hide_yourself/models/Account.java
@@ -1,56 +1,26 @@
 package com.example.hide_yourself.models;
+
+import lombok.*;
+
 import java.sql.Timestamp;
 
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
-    private long userId;
+    private Long userId;
     private String username;
     private String password;
     private String email;
     private Timestamp createdOn;
     private Timestamp lastLogin;
 
-    public Account(){}
-
     public Account(String username, String password, String email) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.createdOn = new Timestamp(System.currentTimeMillis());
-    }
-
-    public long getUserId(){
-        return this.userId;
-    }
-
-    public String getUsername(){
-        return this.username;
-    }
-
-    public String getEmail(){
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Timestamp getCreatedOn() {
-        return this.createdOn;
-    }
-
-    public Timestamp getLastlogin() {
-        return this.lastLogin;
-    }
-
-    public void setLastLogin(Timestamp timestamp) {
-        this.lastLogin = timestamp;
     }
 }
