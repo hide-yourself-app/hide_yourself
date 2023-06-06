ae49fd45893b7509c08654c8935b6a3dec151f0b
Mengqiu (Roger) Chen <rogermchen.ubc@gmail.com>
2023-06-05 20:59:48 -0600

    Create AccountController

---
+++ hide_yourself/src/main/java/com/example/hide_yourself/controllers/AccountController.java
@@ -0,0 +1,9 @@
+package com.example.hide_yourself.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+}
